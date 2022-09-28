17c0546e8c6e73d8db58089289bc59a331a6474b
Tyson G Fritz <tysongfritz@gmail.com>
2022-09-28 16:13:12 -0700

    Cleanup code and show months, weeks, and days remaining.

--- projects/02/life-in-weeks.py
+++ projects/02/life-in-weeks.py
@@ -1,9 +1,12 @@
 typical_lifespan = 87
-weeks_per_year = 52
-weeks_per_life = typical_lifespan * weeks_per_year
 
-user_age = input("How old are you? ")
-user_weeks = int(user_age) * 52
-weeks_remaining = weeks_per_life - user_weeks
+user_age = int(input("How old are you? "))
+user_years_remaining = typical_lifespan - user_age
+months_remaining = user_years_remaining * 12
+weeks_remaining = user_years_remaining * 52
+days_remaining = user_years_remaining * 365
 
-print(f"You have {weeks_remaining} weeks remaining. Use them wisely.")
+
+print(f"You have {months_remaining} months remaining.")
+print(f"You have {weeks_remaining} weeks remaining.")
+print(f"You have {days_remaining} days remaining.")
