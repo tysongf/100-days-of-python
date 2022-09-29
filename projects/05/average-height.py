fe5c0c5d68b97ce523ff412f7a614b9f8b260924
Tyson G Fritz <tysongfritz@gmail.com>
2022-09-29 12:52:05 -0700

    Avg Height

---
+++ projects/05/average-height.py
@@ -0,0 +1,16 @@
+#Average Height = Total Height / Number of Students
+
+total_height = 0
+num_students = 0
+
+heights = input("Enter a list of student heights, separated by a space:\n").split()
+
+for n in range(0, len(heights)):
+   heights[n] = int(heights[n])
+
+for h in heights:
+   total_height += h
+   num_students += 1
+
+print(f"Total Height: {total_height}")
+print(f"Average Height: {total_height / num_students}")
