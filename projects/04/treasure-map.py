36ac45f56c5800d7ac175467ca726d577e9c624e
Tyson G Fritz <tysongfritz@gmail.com>
2022-09-29 12:04:14 -0700

    Treasure Map Grid

---
+++ projects/04/treasure-map.py
@@ -0,0 +1,15 @@
+row1 = ["💼","💼","💼"]
+row2 = ["💼","💼","💼"]
+row3 = ["💼","💼","💼"]
+map = [row1, row2, row3]
+
+print(f"\n{row1}\n{row2}\n{row3}\n")
+
+position = input("Where do you want to put the treasure?\n(00, 01, 02, 10, 11, 12, 20, 21, 22):\n")
+
+row = int(position[0])
+col = int(position[1])
+
+map[row][col] = "💰"
+
+print(f"\n{row1}\n{row2}\n{row3}\n")
