b42923b3add0296ad3eafb8a3eadbda4d36aef99
Tyson G Fritz <tysongfritz@gmail.com>
2022-09-28 22:18:57 -0700

    BMI Calculator

---
+++ projects/03/bmi-calculator.py
@@ -0,0 +1,18 @@
+height = float(input("Enter your height in m: "))
+weight = float(input("Enter your weight in kg: "))
+
+bmi = round(weight / (height **2), 2)
+
+if bmi < 18.5:
+   status = "underweight"
+elif bmi < 25:
+   status = "normal"
+elif bmi < 30:
+   status = "overweight"
+elif bmi < 35:
+   status = "obese"
+else:
+   status = "clinically obese"
+
+print(f"Your BMI is {bmi}")
+print(f"Your weight is: {status}")
