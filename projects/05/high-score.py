ba25b9605647693a8b0e53d272ce8b07ed7d0faf
Tyson G Fritz <tysongfritz@gmail.com>
2022-09-29 12:57:57 -0700

    High Score

---
+++ projects/05/high-score.py
@@ -0,0 +1,10 @@
+
+hi_score = 0
+scores = input("Enter a list of scores, separated by a space:\n").split()
+
+for score in scores:
+   score = int(score)
+   if score > hi_score:
+      hi_score = score
+
+print(f"Highest Score: {hi_score}")
